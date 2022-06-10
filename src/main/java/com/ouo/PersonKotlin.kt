4070f3aac44b9f8d904fbb9b9516952dffd16851
meooooow-o <410570126@gapp.fju.edu.tw>
2022-06-10 16:52:41 +0800

    Hello world with Person Kotlin

---
+++ src/main/java/com/ouo/PersonKotlin.kt
@@ -0,0 +1,13 @@
+package com.ouo
+
+class PersonKotlin {
+    fun greeting() {
+        println("Hello Kotlin")
+    }
+}
+
+//Top-level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+}
