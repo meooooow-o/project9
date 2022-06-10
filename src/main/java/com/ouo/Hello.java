ffe4680374b9043f370241666ddf9ac3a3ef457b
meooooow-o <410570126@gapp.fju.edu.tw>
2022-06-10 16:48:10 +0800

    Hello

---
+++ src/main/java/com/ouo/Hello.java
@@ -0,0 +1,11 @@
+package com.ouo;
+
+public class Hello {
+
+    public static void main(String[] args) {
+//      System.out.println("Hello World!!");
+        Person p = new Person();
+        p.greeting();
+
+    }
+}
