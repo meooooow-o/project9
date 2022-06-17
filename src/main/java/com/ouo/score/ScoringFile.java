8dc663862b09e5f132e87778b72629cea24d471e
meooooow-o <410570126@gapp.fju.edu.tw>
2022-06-17 16:27:34 +0800

    讀取資料

--- src/main/java/com/ouo/score/ScoringFile.java
+++ src/main/java/com/ouo/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package com.ouo.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,12 +9,20 @@ public class ScoringFile {
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("score.txt");
-            int data = fileReader.read();
-            while(data != -1){
-                System.out.print((char)data);
-                data = fileReader.read();
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line = reader.readLine();
+            while(line != null){
+//                System.out.println(line);
+                String[] token = line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name,english,math);
+                stu.print();
+                line = reader.readLine();
             }
-        }catch(IOException e){
+        }catch(Exception e){
             System.out.println("檔案讀取失敗");
         }
         System.out.println("Testing");
